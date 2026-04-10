cbf6879acbe7b5c6fd9720da33a1a1fb2825d421
AaronPauloB <qapvbalderama@tip.edu.ph>
2026-04-10 22:51:58 +0800

    initial

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Status from './components/Status';
+import MessageList from './components/MessageList';
+import Toolbar from './components/Toolbar';
+import InputMethodEditor from './components/InputMethodEditor';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Status />
+      <View style={styles.content}>
+        <MessageList />
+      </View>
+      <View style={styles.toolbar}>
+        <Toolbar />
+      </View>
+      <View style={styles.inputMethodEditor}>
+        <InputMethodEditor />
+      </View>
     </View>
   );
 }
@@ -13,8 +24,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0,0,0,0.04)',
+    backgroundColor: 'white',
   },
 });
