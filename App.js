0cbbeb2c4e00620f6cc68d48b826596235be62c7
AaronPauloB <qapvbalderama@tip.edu.ph>
2026-04-11 16:57:17 +0800

    HOA 12.2

--- App.js
+++ App.js
@@ -1,30 +1,145 @@
-import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, StatusBar, Alert, TouchableHighlight, Image, BackHandler } from 'react-native';
 import Status from './components/Status';
 import MessageList from './components/MessageList';
 import Toolbar from './components/Toolbar';
 import InputMethodEditor from './components/InputMethodEditor';
+import { createImageMessage, createLocationMessage, createTextMessage } from './utils/MessageUtils';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Status />
+export default class App extends React.Component {
+  state = {
+    messages: [
+      createImageMessage('https://unsplash.it/300/300'),
+      createTextMessage('World'),
+      createTextMessage('Hello'),
+      createLocationMessage({
+        latitude: 37.78825,
+        longitude: -122.4324,
+      }),
+    ],
+    fullscreenImageId: null,
+  };
+
+  componentWillMount() {
+    this.subscription = BackHandler.addEventListener('hardwareBackPress', () => {
+      const { fullscreenImageId } = this.state;
+      if (fullscreenImageId) {
+        this.dismissFullscreenImage();
+        return true; // tells Android we handled the back press
+      }
+      return false; // lets Android handle the back press normally
+    });
+  }
+
+  componentWillUnmount() {
+    this.subscription.remove();
+  }
+
+  dismissFullscreenImage = () => {
+    this.setState({ fullscreenImageId: null });
+  };
+
+  handlePressMessage = ({ id, type }) => {
+    switch (type) {
+      case 'text':
+        Alert.alert(
+          'Delete Message',
+          'Are you sure you want to delete this message?',
+          [
+            {
+              text: 'Cancel',
+              style: 'cancel',
+            },
+            {
+              text: 'Delete',
+              style: 'destructive',
+              onPress: () => {
+                this.setState(({ messages }) => ({
+                  messages: messages.filter(message => message.id !== id),
+                }));
+              },
+            },
+          ],
+        );
+        break;
+      case 'image':
+        this.setState({ fullscreenImageId: id });
+        break;
+      default:
+        break;
+    }
+  };
+
+  renderMessageList() {
+    const { messages } = this.state;
+    return (
       <View style={styles.content}>
-        <MessageList />
+        <MessageList
+          messages={messages}
+          onPressMessage={this.handlePressMessage}
+        />
       </View>
+    );
+  }
+
+  renderToolbar() {
+    return (
       <View style={styles.toolbar}>
         <Toolbar />
       </View>
+    );
+  }
+
+  renderInputMethodEditor() {
+    return (
       <View style={styles.inputMethodEditor}>
         <InputMethodEditor />
       </View>
-    </View>
-  );
+    );
+  }
+
+  renderFullscreenImage = () => {
+    const { messages, fullscreenImageId } = this.state;
+
+    if (!fullscreenImageId) return null;
+
+    const image = messages.find(message => message.id === fullscreenImageId);
+
+    if (!image) return null;
+
+    const { uri } = image;
+
+    return (
+      <TouchableHighlight
+        style={styles.fullscreenOverlay}
+        onPress={this.dismissFullscreenImage}
+      >
+        <Image
+          style={styles.fullscreenImage}
+          source={{ uri }}
+        />
+      </TouchableHighlight>
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Status />
+        {this.renderMessageList()}
+        {this.renderToolbar()}
+        {this.renderInputMethodEditor()}
+        {this.renderFullscreenImage()}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+    paddingTop: StatusBar.currentHeight,
   },
   content: {
     flex: 1,
@@ -38,5 +153,14 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderTopColor: 'rgba(0,0,0,0.04)',
     backgroundColor: 'white',
+  },
+  fullscreenOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'black',
+    zIndex: 10,
+  },
+  fullscreenImage: {
+    flex: 1,
+    resizeMode: 'contain',
   },
 });
--- components/MessageList.js
+++ components/MessageList.js
@@ -1,5 +1,110 @@
-import { View } from 'react-native';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { MessageShape } from '../utils/MessageUtils';
 
-export default function MessageList() {
-  return <View />;
+const keyExtractor = item => item.id.toString();
+
+export default class MessageList extends React.Component {
+  static propTypes = {
+    messages: PropTypes.arrayOf(MessageShape).isRequired,
+    onPressMessage: PropTypes.func,
+  };
+
+  static defaultProps = {
+    onPressMessage: () => {},
+  };
+
+  renderMessageBody = ({ type, text, uri, coordinate }) => {
+    switch (type) {
+      case 'text':
+        return (
+          <View style={styles.messageBubble}>
+            <Text style={styles.text}>{text}</Text>
+          </View>
+        );
+      case 'image':
+        return (
+          <Image
+            style={styles.image}
+            source={{ uri }}
+          />
+        );
+      case 'location':
+        return (
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              ...coordinate,
+              latitudeDelta: 0.08,
+              longitudeDelta: 0.04,
+            }}
+          >
+            <Marker coordinate={coordinate} />
+          </MapView>
+  );
+      default:
+        return null;
+    }
+  };
+
+  renderMessageItem = ({ item }) => {
+    const { onPressMessage } = this.props;
+    return (
+      <View key={item.id} style={styles.messageRow}>
+        <TouchableOpacity onPress={() => onPressMessage(item)}>
+          {this.renderMessageBody(item)}
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  render() {
+    const { messages } = this.props;
+    return (
+      <FlatList
+        style={styles.container}
+        inverted
+        data={messages}
+        renderItem={this.renderMessageItem}
+        keyExtractor={keyExtractor}
+        keyboardShouldPersistTaps={'handled'}
+      />
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    overflow: 'visible',
+  },
+  messageRow: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginLeft: 60,
+    marginBottom: 4,
+    marginRight: 10,
+  },
+  messageBubble: {
+    backgroundColor: '#0084FF',
+    borderRadius: 20,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+  },
+  map: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+  },
+});
